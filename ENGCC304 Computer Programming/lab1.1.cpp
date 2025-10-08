3e75ce676d9df9521f43546bd083fc36ba298722
Sougus017 <84127595+PooH017-4@users.noreply.github.com>
2025-10-08 11:55:42 +0700

    Add files via upload

---
+++ ENGCC304 Computer Programming/lab1.1.cpp
@@ -0,0 +1,19 @@
+//Lab 1.1-017-4
+#include <stdio.h>
+
+int main() {
+    int numA = 0 ;
+    int numB = 0 ;
+    int numC = 0 ;
+    int answer = 0;
+    
+    printf( "Enter 1st Number: " ) ;
+    scanf( "%d", &numA ) ;
+    printf( "Enter 2nd Number: " ) ;
+    scanf( "%d", &numB ) ;
+    printf( "Enter 3rd Number: " ) ;
+    scanf( "%d", &numC ) ;
+   
+    printf( "Answer = %d\n", answer = numA+numB+numC ) ;
+    
+}//end main function
